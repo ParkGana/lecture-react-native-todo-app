2f7928e65201deece719f567b6758d8a3b6df210
ParkGana <gnchoco97@gmail.com>
2023-11-28 15:15:00 +0900

    로그인페이지 화면 퍼블리싱

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import MainScreen from './screens/MainScreen'
 import { store } from './redux/store'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
+import SignInScreen from './screens/SignInScreen'
 
 export default function App() {
     const Stack = createNativeStackNavigator()
@@ -11,6 +12,7 @@ export default function App() {
         <Provider store={store}>
             <NavigationContainer>
                 <Stack.Navigator>
+                    <Stack.Screen options={{ headerShown: false }} name="SignIn" component={SignInScreen} />
                     <Stack.Screen options={{ headerShown: false }} name="Main" component={MainScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
